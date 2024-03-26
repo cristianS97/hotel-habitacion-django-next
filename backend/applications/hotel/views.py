a32400eef4f967e57b5155a460a4e54b67953f6a
cristian sáez <44517223+cristianS97@users.noreply.github.com>
2024-03-25 22:24:12 -0300

    Se muestran hoteles registrados en la bbdd

--- backend/applications/hotel/views.py
+++ backend/applications/hotel/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Hotel
 
 # Create your views here.
+class HotelListView(ListView):
+    model=Hotel
+    template_name="hoteles.html"
