6bc5634d2cb7a44ffad174ff6f38940730bdd21b
cristian sáez <44517223+cristianS97@users.noreply.github.com>
2024-03-30 01:02:21 -0300

    Se conecta frontend de hoteles a la api

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Instalaciones
+    "corsheaders",
     # Mis apps
     "applications.hotel",
     "applications.habitacion",
@@ -51,6 +53,9 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # CORS
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = "backend.urls"
@@ -121,3 +126,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ORIGIN_ALLOW_ALL = True
---
+++ frontend/src/app/hotel/page.tsx
@@ -0,0 +1,89 @@
+'use client'
+import { useState, useEffect } from "react";
+
+interface IHotel {
+  "id": number,
+  "nombre": string,
+  "calle": string,
+  "numero": number,
+  "comuna": string,
+  "telefono": number,
+  "email": string
+}
+
+export default function Hotel() {
+  const [hoteles, setHoteles] = useState<IHotel[]>([]);
+
+  useEffect(() => {
+    const getHoteles = () => {
+      fetch("http://localhost:8000/api/hotel")
+        .then(response => response.json())
+        .then(data => setHoteles(data));
+    }
+
+    getHoteles();
+  }, [])
+
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1>Listado de hoteles</h1>
+
+      <div className="relative overflow-x-auto">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Id
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Nombre
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Calle
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Número
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Comuna
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Telefono
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Email
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {hoteles.map(hotel => (
+              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  {hotel["id"]}
+                </th>
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  {hotel["nombre"]}
+                </th>
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  {hotel["calle"]}
+                </th>
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  {hotel["numero"]}
+                </th>
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  {hotel["comuna"]}
+                </th>
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  {hotel["telefono"]}
+                </th>
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  {hotel["email"]}
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </main>
+  );
+}
