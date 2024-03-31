cea80d78c09bb4978f4e33e546ac630a4dc0a8e7
cristian sáez <44517223+cristianS97@users.noreply.github.com>
2024-03-31 20:28:08 -0300

    Se utiliza componentes de tabla personalizados en vista de habitaciones

--- frontend/src/app/habitacion/page.tsx
+++ frontend/src/app/habitacion/page.tsx
@@ -4,6 +4,12 @@ import { useSearchParams } from 'next/navigation';
 import Link from 'next/link';
 import IHabitacion from '@/interfaces/interfaceHabitacion';
 import { useFetchData } from "@/hooks/useFetchData";
+import { Table } from "@/components/Table";
+import { THead } from "@/components/THead";
+import { Row } from "@/components/Row";
+import { TH } from "@/components/TH";
+import { TBody } from "@/components/TBody";
+import { Cell } from "@/components/Cell";
 
 export default function Hotel() {
   const searchParams = useSearchParams();
@@ -19,36 +25,24 @@ export default function Hotel() {
       <h1>Listado de habitaciones<br/><Link className="font-medium text-blue-600 dark:text-blue-500 hover:underline" href={'hotel'}>Volver a hoteles</Link></h1>
 
       <div className="relative overflow-x-auto">
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                Id
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Número
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Ocupada
-              </th>
-            </tr>
-          </thead>
-          <tbody>
+        <Table>
+          <THead>
+            <Row headerRow={true}>
+              <TH value="Id" />
+              <TH value="Número" />
+              <TH value="Ocupada" />
+            </Row>
+          </THead>
+          <TBody>
             {habitaciones.map(habitacion => (
-              <tr key={habitacion.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                  {habitacion.id}
-                </td>
-                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                  {habitacion.numero}
-                </td>
-                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                  {habitacion.ocupado}
-                </td>
-              </tr>
+              <Row key={habitacion.id} headerRow={false}>
+                <Cell value={habitacion.id} />
+                <Cell value={habitacion.numero} />
+                <Cell value={habitacion.ocupado} />
+              </Row>
             ))}
-          </tbody>
-        </table>
+          </TBody>
+        </Table>
       </div>
     </main>
   );
