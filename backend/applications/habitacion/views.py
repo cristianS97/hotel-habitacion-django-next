7d1b2b8b7eb5307fde7bd80d143ef6400fe82a3a
cristian sáez <44517223+cristianS97@users.noreply.github.com>
2024-04-28 15:21:03 -0400

    Se corrige método get_queryset de HabitacionListAPIView

--- backend/applications/habitacion/views.py
+++ backend/applications/habitacion/views.py
@@ -9,11 +9,14 @@ class HabitacionListAPIView(generics.ListCreateAPIView):
     serializer_class = HabitacionSerializer
 
     def get_queryset(self):
-        if len(self.request.GET) > 0:
-            hotel = Hotel.objects.get(pk=self.request.GET["idHotel"])
-            return Habitacion.objects.filter(hotel=hotel)
-        else:
-            return Habitacion.objects.all()
+        id_hotel = self.request.GET.get('idHotel')
+        if id_hotel:
+            try:
+                hotel = Hotel.objects.get(pk=id_hotel)
+                return Habitacion.objects.filter(hotel=hotel)
+            except Hotel.DoesNotExist:
+                return Habitacion.objects.none()
+        return Habitacion.objects.all()
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
