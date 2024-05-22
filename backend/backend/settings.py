c08ca6a65017bf3a430a7993ec11ca1e99713f3b
cristian sáez <44517223+cristianS97@users.noreply.github.com>
2024-05-22 00:33:34 -0400

    Se actualizan nombres de las variables de entorno

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": os.environ.get('POSTGRES_DB', 'hotel'),
         "USER": os.environ.get('POSTGRES_USER', 'postgres'),
         "PASSWORD": os.environ.get('POSTGRES_PASSWORD', 'qwerty'),
-        "HOST": os.environ.get('POSTGRES_SERVER', 'localhost'),
+        "HOST": os.environ.get('POSTGRES_SERVER', 'postgres'),
         "PORT": os.environ.get('POSTGRES_PORT', 5432)
     }
 }
